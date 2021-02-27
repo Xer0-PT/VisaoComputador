0d18c00bc26d40fe6fed89528e299c1954f52c5f
Xer0-PT <joelflm@gmail.com>
2021-02-27 14:11:11 +0000

    Ex 4 done

--- ex4.c
+++ ex4.c
@@ -4,11 +4,13 @@
 int main(void)
 {
     IVC *image;
-    int i;
-    int size = 256;
+    int i, x, y;
+    int width = 256;
+    int height = 256;
     int color = 0;
+    long int pos;
 
-    image = vc_image_new(size, size, 1, 1);
+    image = vc_image_new(width, height, 1, 255);
 
     if (image == NULL)
     {
@@ -17,13 +19,47 @@ int main(void)
         return 0;
     }
 
-    for ( i = 0; i < size*size; i++)
+    // 1ª Imagem
+
+/* 
+    for (i = 0; i < width*height; i++)
     {
         image->data[i] = color;
         color++;
     }
-    
-    vc_write_image("ex4.pbm", image);
+    vc_write_image("ex4.1.pbm", image);
+ */
+
+    // 2ª Imagem
+/* 
+    for (x = 0; x < width; x++)
+    {
+        for (y = 0; y < height; y++)
+        {
+            pos = y * image->bytesperline + x * image->channels;
+            image->data[pos] = color;
+            color++;
+        }
+    }
+    vc_write_image("ex4.2.pbm", image);
+ */
+
+    // 3ª Imagem
+    // André Ferreira
+
+    for (x = 0; x < width; x++)
+    {
+        for (y = 0; y < height; y++)
+        {
+            pos = y * image->bytesperline + x * image->channels;
+            image->data[pos] = color/2;
+            color++;
+        }
+        color = x+1; //re-increments level
+    }    
+    vc_write_image("ex4.3.pbm", image);
+
+
 
     vc_image_free(image);
 
